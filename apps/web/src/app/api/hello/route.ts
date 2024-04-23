0e98fa0bffb1c05222d44a20c29a3ddaa6cca61c
Anthony <AnthonySgro1995@gmail.com>
2024-04-23 02:37:50 -0400

    Add test api route

---
+++ apps/web/src/app/api/hello/route.ts
@@ -0,0 +1,19 @@
+// READ MORE: https://nextjs.org/docs/app/building-your-application/routing/route-handlers
+
+import { NextRequest, NextResponse } from "next/server";
+
+// To handle a GET request to /api
+// eslint-disable-next-line no-unused-vars
+export async function GET(req: NextRequest) {
+  // Do whatever you want
+  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+}
+
+// To handle a POST request to /api
+// eslint-disable-next-line no-unused-vars
+export async function POST(req: NextRequest) {
+  // Do whatever you want
+  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+}
+
+// Same logic to add a `PATCH`, `DELETE`...
